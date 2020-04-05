5935d68517785005868054bad4163e4faeed7b91
Agath <agathe.lebouler@gmail.com>
2020-04-05 14:59:13 +0200

    50% errors, 200 differences for W, 120 differences for w

--- devoir4/solution_ocr.py
+++ devoir4/solution_ocr.py
@@ -27,22 +27,33 @@ class ReseauDeNeurones:
         return self.W, self.w
 
     def prediction(self, x):
-        scores = np.dot(self.W, x)  # TODO: add bias?
-        sum_scores = np.sum(scores)
-        h_t = logistic(sum_scores)
-        prediction = 1 if h_t >= 0.5 else 0
+        # TODO: add bias?
+        scores_couche_1 = np.dot(self.W, x)
+        h_t_couche_1 = logistic(scores_couche_1)
+
+        scores_couche_2 = np.dot(self.w, h_t_couche_1)
+        h_t_couche_2 = logistic(scores_couche_2)
+
+        prediction = 1 if h_t_couche_2 >= 0.5 else 0
         return prediction
 
     def mise_a_jour(self, x, y):
-        h_t = logistic(x)
-        self.W += self.alpha * (y - h_t) * x
+        scores_couche_1 = np.dot(self.W, x)
+        h_t_couche_1 = logistic(scores_couche_1)
+
+        scores_couche_2 = np.dot(self.w, h_t_couche_1)
+        h_t_couche_2 = logistic(scores_couche_2)
+
+        grad_output = y - h_t_couche_2
+        self.w = h_t_couche_1 * (y - h_t_couche_1) * self.w * grad_output
+
+        for i, x_i in enumerate(x):
+            self.W[:, i] = x_i * (y - x_i) * self.W[:, i] * self.w
+
+
 
     def entrainement(self, X, Y):
         for t in range(self.T):
-            for index, (x_t, y_t) in enumerate(zip(X, Y)):
-                scores = np.dot(self.W, x_t)  # TODO: add bias?
-                sum_scores = np.sum(scores)
-                h_t = logistic(sum_scores)
-                prediction = 1 if h_t >= 0.5 else 0
-                if prediction != y_t:
+            for x_t, y_t in zip(X, Y):
+                if self.prediction(x_t) != y_t:
                     self.mise_a_jour(x_t, y_t)
